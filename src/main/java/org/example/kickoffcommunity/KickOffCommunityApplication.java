2befe051031633b0ec982495627d927f0ab46e78
dream2405 <dream2405@naver.com>
2024-04-09 18:23:24 +0900

    README 파일 추가

--- src/main/java/org/example/kickoffcommunity/KickOffCommunityApplication.java
+++ src/main/java/org/example/kickoffcommunity/KickOffCommunityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KickOffCommunityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KickOffCommunityApplication.class, args);
 		System.out.println("asdfsa");
