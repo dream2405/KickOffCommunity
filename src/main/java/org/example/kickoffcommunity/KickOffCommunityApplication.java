cf31a1762bde6d4f8317fc11fec8a7798792cd96
dream2405 <dream2405@naver.com>
2024-04-09 17:30:40 +0900

    첫번째 생성

---
+++ src/main/java/org/example/kickoffcommunity/KickOffCommunityApplication.java
@@ -0,0 +1,13 @@
+package org.example.kickoffcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickOffCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickOffCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/kickoffcommunity/KickOffCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kickoffcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickOffCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
